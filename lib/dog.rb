55046303f323466ad8efed55074235c2798efe77
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-20 17:19:36 +0300

    Done

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+    # Bark
+    def bark
+        puts 'Woof!'
+    end
+
+    # Sit
+    def sit 
+        puts 'The Dog is sitting'
+    end
+end
+
+lassie = Dog.new
+lassie.bark
+lassie.sit
